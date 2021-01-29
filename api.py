717bf23e82ce4dd326ff7ae36a3615dd44abb61b
ishikamen85 <77919100+ishikamen85@users.noreply.github.com>
2021-01-29 19:32:57 -0300

    Revisão do código html.

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ import json
 import jsonify
 from flask import render_template
 
+
 app = flask.Flask(__name__,template_folder='./templates/')
 app.config["DEBUG"] = True
 
--- static/js/main.js
+++ static/js/main.js
@@ -41,4 +41,25 @@
 
 	});
 
+var wrapper_div = document.getElementById('input_set');
+var btn = document.getElementById('btn');
+
+btn.addEventListener('click', function() {
+  var n = document.getElementById("no_of_fields").value;
+  var fieldset = document.createElement('div'),
+    newInput;
+  for (var k = 0; k < n; k++) {
+
+    newInput = document.createElement('input');
+    newInput.value = '';
+    newInput.type = 'text';
+    newInput.placeholder = "Textfield no. " + k;
+    fieldset.appendChild(newInput);
+    fieldset.appendChild(document.createElement('br'));
+  }
+
+  wrapper_div.insertBefore(fieldset, this);
+
+}, false);
+
 })(jQuery);
